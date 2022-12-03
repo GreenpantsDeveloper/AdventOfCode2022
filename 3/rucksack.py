e3ed27e07fe019707680edef46dcfef7f337309e
Herman Groenbroek <herman@hermans-macbook.home>
2022-12-03 12:02:18 +0100

    Add solutions to the challenge of day 3 :-)

---
+++ 3/rucksack.py
@@ -0,0 +1,39 @@
+def parse_input(filename='input.txt'):
+    """
+    Parse the rucksacks input file.
+
+    :param filename: input filename.
+    :return: rucksacks and their compartments.
+    """
+    with open(filename, 'r') as fp:
+        rucksacks = [line.strip() for line in fp.readlines()]
+
+    compartments = [(items[:len(items) // 2], items[len(items) // 2:]) for items in rucksacks]
+
+    return compartments
+
+
+def prio(char):
+    """
+    Get the priority of a character.
+
+    :return: priority of a character.
+    """
+    if 'a' <= char <= 'z':
+        return 1 + ord(char) - ord('a')
+    if 'A' <= char <= 'Z':
+        return 27 + ord(char) - ord('A')
+    else:
+        raise ValueError("Unsupported rucksack item:", char)
+
+
+if __name__ == '__main__':
+    # Get rucksacks split by compartments
+    rucksacks = parse_input()
+
+    # Find the item that appears in both compartments.
+    overlapping_items = [set(rucksack[0]) & set(rucksack[1]) for rucksack in rucksacks]
+
+    # Compute the summed priorities.
+    summed_prio = sum([prio(item_set.pop()) for item_set in overlapping_items])
+    print(f"The sum of the priorities of overlapping compartment items is {summed_prio}.")
---
+++ 3/rucksack_pt2.py
@@ -0,0 +1,40 @@
+from rucksack import prio
+
+GROUP_SIZE = 3
+
+
+def parse_input(filename='input.txt'):
+    """
+    Parse the rucksacks input file for part 2.
+
+    :param filename: input filename.
+    :return: rucksacks split by groups.
+    """
+    # Read the file.
+    with open(filename, 'r') as fp:
+        rucksacks = [line.strip() for line in fp.readlines()]
+
+    # Group by three lines.
+    return [rucksacks[i:i + GROUP_SIZE] for i in range(0, len(rucksacks), GROUP_SIZE)]
+
+
+def find_badge(group):
+    """
+    Find an elf group's badge: the one overlapping item.
+
+    :param group: Three rucksacks as a group of elves.
+    :return: The matching badge item.
+    """
+    return (set(group[0]) & set(group[1]) & set(group[2])).pop()
+
+
+if __name__ == '__main__':
+    # Split rucksacks by groups.
+    groups = parse_input()
+
+    # Find the groups' badges.
+    badges = [find_badge(group) for group in groups]
+
+    # Compute the summed priorities for the badges.
+    summed_prio = sum([prio(badge) for badge in badges])
+    print(f"The sum of the priorities of the groups' badges is {summed_prio}.")
